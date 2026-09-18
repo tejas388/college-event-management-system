f9def6ebca38f3977558f84d9f92cf713c3573b4
Tejas C K <tejas2005patel@gmail.com>
2026-09-18 17:14:55 +0530

    Update database configuration

--- app/config.php
+++ app/config.php
@@ -8,7 +8,7 @@ define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '8889');
 define('DB_NAME', 'college_events');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', 'YOUR_LOCAL_MAMP_PASSWORD');
 
 // Base URL for the application
 define('BASE_URL', 'http://localhost:8888/myproject/college-events/public/');
